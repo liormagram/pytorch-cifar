2f2420dec90be67107dc8e8026d219c8d23033df
Lior <lior.magram10@gmail.com>
2020-04-15 14:31:43 +0300

    running l2 norm

--- models/vgg.py
+++ models/vgg.py
@@ -32,8 +32,8 @@ class VGG(nn.Module):
                 layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
             else:
                 layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
-                           nn.BatchNorm2d(x),
-                           # MyBatchNorm2d(x),
+                           # nn.BatchNorm2d(x),
+                           MyBatchNorm2d(x),
                            nn.ReLU(inplace=True)]
                 in_channels = x
         layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
