41023613410d5ebc3c74b7998c6c801b8ca9b130
Lior <lior.magram10@gmail.com>
2020-04-18 19:59:40 +0300

    adding l1 norm

--- my_layers.py
+++ my_layers.py
@@ -8,6 +8,7 @@ class MyBatchNorm2d(nn.BatchNorm2d):
             num_features, eps, momentum, affine, track_running_stats)
         # self.running_l2 = torch.zeros(num_features).to('cuda:0')
         self.running_l2 = torch.zeros(num_features)
+        self.running_l1 = torch.zeros(num_features)
         a=1
 
     def forward(self, input):
@@ -30,28 +31,34 @@ class MyBatchNorm2d(nn.BatchNorm2d):
             # use biased var in train
             var = input.var([0, 2, 3], unbiased=False)
             l2 = (input-mean[None, :, None, None]).norm(2, [0, 2, 3])/torch.sqrt(torch.tensor([n-1]).to('cuda'))
+            l1 = (input-mean[None, :, None, None]).norm(1, [0, 2, 3])/(torch.tensor([n-1]).to('cuda'))
 
             with torch.no_grad():
                 self.running_mean = exponential_average_factor * mean\
                     + (1 - exponential_average_factor) * self.running_mean
                 # update running_var with unbiased var
-                self.running_var = (exponential_average_factor * var * n / (n - 1)\
-                    + (1 - exponential_average_factor) * self.running_var).to('cuda')
+                # self.running_var = (exponential_average_factor * var * n / (n - 1)\
+                #     + (1 - exponential_average_factor) * self.running_var).to('cuda')
 
                 self.running_l2 = (exponential_average_factor * l2.to('cuda') * n / (n - 1)\
                     + (1 - exponential_average_factor) * self.running_l2.to('cuda')).to('cuda')
+
+                self.running_l2 = (exponential_average_factor * l1.to('cuda') * n / (n - 1) \
+                    + (1 - exponential_average_factor) * self.running_l1.to('cuda')).to('cuda')
         else:
             mean = self.running_mean
-            var = self.running_var
+            # var = self.running_var
             l2 = self.running_l2
+            l1 = self.running_l1
 
-        input_var = ((input - mean[None, :, None, None]) / (torch.sqrt(var[None, :, None, None] + self.eps))).to('cuda')
-        input = ((input - mean[None, :, None, None]) / (l2[None, :, None, None] + self.eps)).to('cuda')
+        # input_var = ((input - mean[None, :, None, None]) / (torch.sqrt(var[None, :, None, None] + self.eps))).to('cuda')
+        # input = ((input - mean[None, :, None, None]) / (l2[None, :, None, None] + self.eps)).to('cuda')
+        input = ((input - mean[None, :, None, None]) / (l1[None, :, None, None] + self.eps)).to('cuda')
 
-        print('input: ')
-        print(input)
-        print('input_var: ')
-        print(input_var)
+        # print('input: ')
+        # print(input)
+        # print('input_var: ')
+        # print(input_var)
         # input = (input - mean[None, :, None, None]) / (torch.norm(input[None, :, None, None], 2) + self.eps)
         if self.affine:
             input = input * self.weight[None, :, None, None] + self.bias[None, :, None, None]
