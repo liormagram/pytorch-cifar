af54d2beb9b2324b25473b475375e829ece89542
Lior <lior.magram10@gmail.com>
2020-04-18 18:53:23 +0300

    correcting l2 norm

--- main.py
+++ main.py
@@ -154,6 +154,6 @@ def test(epoch):
         best_acc = acc
 
 
-for epoch in range(start_epoch, start_epoch+20):
+for epoch in range(start_epoch, start_epoch+200):
     train(epoch)
     test(epoch)
--- my_layers.py
+++ my_layers.py
@@ -6,7 +6,8 @@ class MyBatchNorm2d(nn.BatchNorm2d):
                  affine=True, track_running_stats=True):
         super(MyBatchNorm2d, self).__init__(
             num_features, eps, momentum, affine, track_running_stats)
-        self.running_l2 = torch.zeros(num_features).to('cuda:0')
+        # self.running_l2 = torch.zeros(num_features).to('cuda:0')
+        self.running_l2 = torch.zeros(num_features)
         a=1
 
     def forward(self, input):
@@ -24,27 +25,28 @@ class MyBatchNorm2d(nn.BatchNorm2d):
 
         # calculate running estimates
         if self.training:
+            n = input.numel() / input.size(1)
             mean = input.mean([0, 2, 3])
             # use biased var in train
-            # var = input.var([0, 2, 3], unbiased=False)
-            l2 = input.norm(2, [0, 2, 3])
-            n = input.numel() / input.size(1)
+            var = input.var([0, 2, 3], unbiased=False)
+            l2 = (input-mean[None, :, None, None]).norm(2, [0, 2, 3])/torch.sqrt(torch.tensor([n-1]))
+
             with torch.no_grad():
                 self.running_mean = exponential_average_factor * mean\
                     + (1 - exponential_average_factor) * self.running_mean
                 # update running_var with unbiased var
-                # self.running_var = exponential_average_factor * var * n / (n - 1)\
-                #     + (1 - exponential_average_factor) * self.running_var
+                self.running_var = exponential_average_factor * var * n / (n - 1)\
+                    + (1 - exponential_average_factor) * self.running_var
 
                 self.running_l2 = exponential_average_factor * l2 * n / (n - 1)\
                     + (1 - exponential_average_factor) * self.running_l2
         else:
             mean = self.running_mean
-            # var = self.running_var
+            var = self.running_var
             l2 = self.running_l2
 
+        input_var = (input - mean[None, :, None, None]) / (torch.sqrt(var[None, :, None, None] + self.eps))
         input = (input - mean[None, :, None, None]) / (l2[None, :, None, None] + self.eps)
-        # input = (input - mean[None, :, None, None]) / (torch.sqrt(var[None, :, None, None] + self.eps))
         # input = (input - mean[None, :, None, None]) / (torch.norm(input[None, :, None, None], 2) + self.eps)
         if self.affine:
             input = input * self.weight[None, :, None, None] + self.bias[None, :, None, None]
