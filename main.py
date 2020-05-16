c7125bd6c3d62b91c72152d18e506387d2ef9687
Lior <lior.magram10@gmail.com>
2020-05-16 13:36:24 +0300

    adding BN and ST to lp norms

--- main.py
+++ main.py
@@ -183,8 +183,8 @@ def main_nets():
     nets = []
     norms = [1, 2, 3, 4, 5, 10]
     other_norms = ['BN', 'ST']
-    # for i in range(len(norms)):
-    #     nets.append(VGG('VGG11', norm_type='LP', lp_norm=norms[i], device=device).to(device))
+    for i in range(len(norms)):
+        nets.append(VGG('VGG11', norm_type='LP', lp_norm=norms[i], device=device).to(device))
 
     for norm_type in other_norms:
         nets.append(VGG('VGG11', norm_type=norm_type, device=device).to(device))
