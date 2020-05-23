35160a8504adc386dea331b88a4168b02f195d82
Lior <lior.magram10@gmail.com>
2020-05-23 11:53:23 +0300

    adding googlelenet

--- main.py
+++ main.py
@@ -186,12 +186,14 @@ def main_nets():
     for i in range(len(norms)):
         # nets.append(VGG('VGG11', norm_type='LP', lp_norm=norms[i], device=device).to(device))
         # nets.append(ResNet18(norm_type='LP', lp_norm=norms[i], device=device).to(device))
-        nets.append(EfficientNetB0(norm_type='LP', lp_norm=norms[i], device=device).to(device))
+        # nets.append(EfficientNetB0(norm_type='LP', lp_norm=norms[i], device=device).to(device))
+        nets.append(GoogLeNet(norm_type='LP', lp_norm=norms[i], device=device).to(device))
 
     for norm_type in other_norms:
         # nets.append(VGG('VGG11', norm_type=norm_type, device=device).to(device))
         # nets.append(ResNet18(norm_type=norm_type, device=device).to(device))
-        nets.append(EfficientNetB0(norm_type=norm_type, device=device).to(device))
+        # nets.append(EfficientNetB0(norm_type=norm_type, device=device).to(device))
+        nets.append(GoogLeNet(norm_type=norm_type, device=device).to(device))
 
     net_names = ['l'+str(x) for x in norms] + other_norms
     for i in range(len(nets)):
--- models/googlenet.py
+++ models/googlenet.py
@@ -2,38 +2,51 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from my_layers import norm
 
 
 class Inception(nn.Module):
-    def __init__(self, in_planes, n1x1, n3x3red, n3x3, n5x5red, n5x5, pool_planes):
+    def __init__(self, in_planes, n1x1, n3x3red, n3x3, n5x5red, n5x5, pool_planes,
+                 norm_type='BN', lp_norm=2, device='cpu'):
         super(Inception, self).__init__()
+
+        self.norm_type = norm_type
+        self.lp_norm = lp_norm
+        self.device = device
+
         # 1x1 conv branch
         self.b1 = nn.Sequential(
             nn.Conv2d(in_planes, n1x1, kernel_size=1),
-            nn.BatchNorm2d(n1x1),
+            # nn.BatchNorm2d(n1x1),
+            norm(norm_type=self.norm_type, num_features=n1x1, lp_norm=self.lp_norm, device=self.device),
             nn.ReLU(True),
         )
 
         # 1x1 conv -> 3x3 conv branch
         self.b2 = nn.Sequential(
             nn.Conv2d(in_planes, n3x3red, kernel_size=1),
-            nn.BatchNorm2d(n3x3red),
+            # nn.BatchNorm2d(n3x3red),
+            norm(norm_type=self.norm_type, num_features=n3x3red, lp_norm=self.lp_norm, device=self.device),
             nn.ReLU(True),
             nn.Conv2d(n3x3red, n3x3, kernel_size=3, padding=1),
-            nn.BatchNorm2d(n3x3),
+            # nn.BatchNorm2d(n3x3),
+            norm(norm_type=self.norm_type, num_features=n3x3, lp_norm=self.lp_norm, device=self.device),
             nn.ReLU(True),
         )
 
         # 1x1 conv -> 5x5 conv branch
         self.b3 = nn.Sequential(
             nn.Conv2d(in_planes, n5x5red, kernel_size=1),
-            nn.BatchNorm2d(n5x5red),
+            # nn.BatchNorm2d(n5x5red),
+            norm(norm_type=self.norm_type, num_features=n5x5red, lp_norm=self.lp_norm, device=self.device),
             nn.ReLU(True),
             nn.Conv2d(n5x5red, n5x5, kernel_size=3, padding=1),
-            nn.BatchNorm2d(n5x5),
+            # nn.BatchNorm2d(n5x5),
+            norm(norm_type=self.norm_type, num_features=n5x5, lp_norm=self.lp_norm, device=self.device),
             nn.ReLU(True),
             nn.Conv2d(n5x5, n5x5, kernel_size=3, padding=1),
-            nn.BatchNorm2d(n5x5),
+            # nn.BatchNorm2d(n5x5),
+            norm(norm_type=self.norm_type, num_features=n5x5, lp_norm=self.lp_norm, device=self.device),
             nn.ReLU(True),
         )
 
@@ -41,7 +54,8 @@ class Inception(nn.Module):
         self.b4 = nn.Sequential(
             nn.MaxPool2d(3, stride=1, padding=1),
             nn.Conv2d(in_planes, pool_planes, kernel_size=1),
-            nn.BatchNorm2d(pool_planes),
+            # nn.BatchNorm2d(pool_planes),
+            norm(norm_type=self.norm_type, num_features=pool_planes, lp_norm=self.lp_norm, device=self.device),
             nn.ReLU(True),
         )
 
@@ -54,27 +68,33 @@ class Inception(nn.Module):
 
 
 class GoogLeNet(nn.Module):
-    def __init__(self):
+    def __init__(self, norm_type='BN', lp_norm=2, device='cpu'):
         super(GoogLeNet, self).__init__()
+
+        self.norm_type = norm_type
+        self.lp_norm = lp_norm
+        self.device = device
+
         self.pre_layers = nn.Sequential(
             nn.Conv2d(3, 192, kernel_size=3, padding=1),
-            nn.BatchNorm2d(192),
+            # nn.BatchNorm2d(192),
+            norm(norm_type=self.norm_type, num_features=192, lp_norm=self.lp_norm, device=self.device),
             nn.ReLU(True),
         )
 
-        self.a3 = Inception(192,  64,  96, 128, 16, 32, 32)
-        self.b3 = Inception(256, 128, 128, 192, 32, 96, 64)
+        self.a3 = Inception(192,  64,  96, 128, 16, 32, 32, norm_type=self.norm_type, lp_norm=self.lp_norm, device=self.device)
+        self.b3 = Inception(256, 128, 128, 192, 32, 96, 64, norm_type=self.norm_type, lp_norm=self.lp_norm, device=self.device)
 
         self.maxpool = nn.MaxPool2d(3, stride=2, padding=1)
 
-        self.a4 = Inception(480, 192,  96, 208, 16,  48,  64)
-        self.b4 = Inception(512, 160, 112, 224, 24,  64,  64)
-        self.c4 = Inception(512, 128, 128, 256, 24,  64,  64)
-        self.d4 = Inception(512, 112, 144, 288, 32,  64,  64)
-        self.e4 = Inception(528, 256, 160, 320, 32, 128, 128)
+        self.a4 = Inception(480, 192,  96, 208, 16,  48,  64, norm_type=self.norm_type, lp_norm=self.lp_norm, device=self.device)
+        self.b4 = Inception(512, 160, 112, 224, 24,  64,  64, norm_type=self.norm_type, lp_norm=self.lp_norm, device=self.device)
+        self.c4 = Inception(512, 128, 128, 256, 24,  64,  64, norm_type=self.norm_type, lp_norm=self.lp_norm, device=self.device)
+        self.d4 = Inception(512, 112, 144, 288, 32,  64,  64, norm_type=self.norm_type, lp_norm=self.lp_norm, device=self.device)
+        self.e4 = Inception(528, 256, 160, 320, 32, 128, 128, norm_type=self.norm_type, lp_norm=self.lp_norm, device=self.device)
 
-        self.a5 = Inception(832, 256, 160, 320, 32, 128, 128)
-        self.b5 = Inception(832, 384, 192, 384, 48, 128, 128)
+        self.a5 = Inception(832, 256, 160, 320, 32, 128, 128, norm_type=self.norm_type, lp_norm=self.lp_norm, device=self.device)
+        self.b5 = Inception(832, 384, 192, 384, 48, 128, 128, norm_type=self.norm_type, lp_norm=self.lp_norm, device=self.device)
 
         self.avgpool = nn.AvgPool2d(8, stride=1)
         self.linear = nn.Linear(1024, 10)
@@ -98,10 +118,10 @@ class GoogLeNet(nn.Module):
         return out
 
 
-def test():
-    net = GoogLeNet()
-    x = torch.randn(1,3,32,32)
-    y = net(x)
-    print(y.size())
+# def test():
+#     net = GoogLeNet()
+#     x = torch.randn(1,3,32,32)
+#     y = net(x)
+#     print(y.size())
 
 # test()
